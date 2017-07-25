db5796d4ce8ea0e31b6c0755b783fee9749bea3a
Kav Singh <flakisms@gmail.com>
2017-07-25 19:05:19 +1000

    remove style rules in favour of prettier config.

--- rules/base.js
+++ rules/base.js
@@ -2,28 +2,12 @@ const { ERROR, WARN, OFF } = require('./constants')
 
 module.exports = {
   rules: {
-    semi: [ERROR, 'never'],
     complexity: [WARN, 10],
     'no-console': OFF,
     'no-await-in-loop': [ERROR],
-    'arrow-parens': [ERROR, 'as-needed'],
     'no-use-before-define': [ERROR, 'nofunc'],
     'no-underscore-dangle': [WARN],
     'max-nested-callbacks': [WARN, 5],
-    'max-len': [
-      WARN,
-      {
-        code: 80,
-        tabWidth: 2,
-        ignoreComments: true,
-        ignoreTrailingComments: true,
-        ignoreUrls: true,
-        ignoreStrings: true,
-        ignoreTemplateLiterals: true,
-        ignoreRegExpLiterals: true,
-      },
-    ],
-    'generator-star-spacing': [ERROR, { before: true, after: true }],
     'no-restricted-globals': ['error', 'find', 'self', 'event'],
   },
 }
