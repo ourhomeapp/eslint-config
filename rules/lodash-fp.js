17d4ef21a85df0320ddcf477b77f04828214d122
Kav Singh <flakisms@gmail.com>
2017-07-25 18:57:50 +1000

    update deps. make fewer assumptions about technologies used (e.g. webpack, lodash)

--- rules/lodash-fp.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  plugins: ['lodash-fp'],
-  extends: ['plugin:lodash-fp/recommended'],
-  rules: {
-    'lodash-fp/consistent-compose': ['error', 'pipe'],
-    'lodash-fp/no-unused-result': 'off',
-  },
-}
