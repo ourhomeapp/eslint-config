ac78ff0b843d135dd29aef80ff39f5644fe54e58
Kav Singh <flakisms@gmail.com>
2017-07-25 22:03:16 +1000

    Remove some rules that are no different from airbnb or are better handled per project.

--- rules/base.js
+++ rules/base.js
@@ -1,12 +1,8 @@
-const { ERROR, WARN, OFF } = require('./constants')
+const { ERROR, WARN } = require('./constants')
 
 module.exports = {
   rules: {
     complexity: [WARN, 10],
-    'no-console': OFF,
-    'no-await-in-loop': [ERROR],
-    'no-use-before-define': [ERROR, 'nofunc'],
-    'no-underscore-dangle': [WARN],
     'max-nested-callbacks': [WARN, 5],
     'no-restricted-globals': [
       ERROR,
