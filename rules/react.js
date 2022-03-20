c34fa751cfd8fa4860d9b6ed729a635046df7e0c
Kav Singh <flakisms@gmail.com>
2022-03-20 23:39:39 +0100

    allow react props spread, v32.3.0

--- rules/react.js
+++ rules/react.js
@@ -10,6 +10,7 @@ module.exports = {
         unnamedComponents: 'arrow-function',
       },
     ],
+    'react/jsx-props-no-spreading': 'off',
     'react/jsx-uses-react': 'off',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
