42faf7c74074c6c00d4e8ea3101badb61de6b2b4
Kav Singh <flakisms@gmail.com>
2022-11-27 20:15:58 +0100

    update deps, v37.6.0

--- test/samples/validateConfig.ts
+++ test/samples/validateConfig.ts
@@ -10,14 +10,6 @@ enum Foo {
 
 const withFoo = (f: Foo, cb: (f: Foo) => void): void => cb(f);
 
-const _ = 5;
+export type Ident = <T>(foo: T) => T;
 
-const ident: {
-  // testing syntax for type generics in overload
-  // eslint-disable-next-line @typescript-eslint/prefer-function-type
-  <T>(_: T): T;
-} = (t) => t;
-
-const ident2: <T>(_: T) => T = (t) => t;
-
-export { _, withFoo, ident, ident2 };
+export { foo, withFoo };
